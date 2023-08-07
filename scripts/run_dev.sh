a3d02ce8adb5f6dcbd15040edbf2e2d795a722fb
samcts2309 <sam.choy@noveltebot.com>
2023-08-07 17:09:23 +0800

    update run script

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -15,10 +15,26 @@ DOCKER_ARGS+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
 if [[ $PLATFORM == "aarch64" ]]; then
     DOCKER_ARGS+=("-v /usr/bin/tegrastats:/usr/bin/tegrastats")
     DOCKER_ARGS+=("-v /tmp/argus_socket:/tmp/argus_socket")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h:/usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h")
     DOCKER_ARGS+=("-v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra")
     DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
     DOCKER_ARGS+=("-v /opt/nvidia/nsight-systems-cli:/opt/nvidia/nsight-systems-cli")
     DOCKER_ARGS+=("--pid=host")
+    DOCKER_ARGS+=("-v /opt/nvidia/vpi2:/opt/nvidia/vpi2")
+    DOCKER_ARGS+=("-v /usr/share/vpi2:/usr/share/vpi2")
+
+    # If jtop present, give the container access
+    if [[ $(getent group jtop) ]]; then
+        DOCKER_ARGS+=("-v /run/jtop.sock:/run/jtop.sock:ro")
+        JETSON_STATS_GID="$(getent group jtop | cut -d: -f3)"
+        DOCKER_ARGS+=("--group-add $JETSON_STATS_GID")
+    fi
 fi
 
 CONTAINER_NAME="jetson_dev-container"
@@ -28,11 +44,13 @@ print_info "Running $CONTAINER_NAME"
 shift 2
 print_info $@
 docker run -it \
-    --privileged --network host \
+    --privileged \
+    --network host \
     ${DOCKER_ARGS[@]} \
     -v $DEV_DIR:/workspaces/jetson-dev \
-    -v /dev:/dev \
+    -v /dev/*:/dev/* \
     -v /var/novelte:/var/novelte \
+    -v /etc/localtime:/etc/localtime:ro \
     --name "$CONTAINER_NAME" \
     --runtime nvidia \
     --workdir /workspaces/jetson-dev \
